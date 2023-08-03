ada3bf235a11668e6ee230c9ffd95bcf4cc997d6
abbosmakhmudov <abbasmakhmudov@gmail.com>
2023-08-02 20:05:20 -0400

    first commit

---
+++ src/E1.java
@@ -0,0 +1,11 @@
+public class E1 {
+    public static void main(String[] args) {
+        boolean productavailable=true;
+        if(productavailable==true)
+            System.out.println("Yay");
+        else {
+            System.out.println("Noo");
+
+        }
+    }
+}
